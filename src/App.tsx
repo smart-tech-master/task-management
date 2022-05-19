b05f66650a0798f1d2b1b70d9103157a02214293
kevinpreston <kpreston313@gmail.com>
2022-05-19 16:46:51 -0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+let name: string;
+let age: number | string;
+let isStudent: boolean;
+let hobbies: string[];
+let role: [number, string];
+
+// let printName: Function;
+
+function printName(name:string) {
+  console.log(name);
+}
+
+printName("TSK");
+// type Person = {
+//   name: string,
+//   age?: number
+// };
+
+// let person: Person = {
+//   name: "SongGil",
+// }
+
+// let lotsOfPeople: Person[];
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello World
     </div>
   );
 }
